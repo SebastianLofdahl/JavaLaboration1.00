437d03768cf894771692692d370434b258aa1a22
SebastianLofdahl <randomgametales@gmail.com>
2026-09-23 10:09:56 +0200

    Laboration ett. Ändring 10:09 23/8-26, extra kommentarer

--- src/main/java/Se/iths/sebastian/laborationEtt/laborationDice/model/Game.java
+++ src/main/java/Se/iths/sebastian/laborationEtt/laborationDice/model/Game.java
@@ -6,6 +6,8 @@ public class Game {
         //Min public metod för att kunna kalla på showMenu och dens följande funktioner till Main
         showMenu();
     }
+    //Hade problem att få menyn in i egen klass stod först i Main, sen att jag behövde lägga in den i
+    //"public Play" tog lite tid att lista ut
 
     private void showMenu() {
         /*Skapade en startmeny med en vanlig switch, tog lite googlande för att komma ihåg exakt hur de funka
@@ -21,17 +23,19 @@ public class Game {
             String choice = IO.readln("Ditt val (1-3): ");
 
             switch (choice) {
-                case "1" -> {
+                case "1" -> { //Om man väljer att spela så ska case 1 då skapa 2 spelare, slå tärningarna och
+                    //visa resultatet
                     Player playerOne = createPlayer(1);
                     Player playerTwo = createPlayer(2);
                     playRound(playerOne);
                     playRound(playerTwo);
                     printResult(playerOne, playerTwo);
                 }
-                case "2" ->
+                case "2" -> //Förklarar regler, bara en iop för detta
                         IO.println("Varje spelare slår tärningen två gånger på rad, sedan räknas summorna ihop och högsta resultat vinner.");
-                case "3" -> IO.println(inMenu = false);
+                case "3" -> IO.println(inMenu = false); //Avslutar spelet
                 default -> IO.println("Error: Ogiltigt val! Måste göra ett val mellan 1-3.");
+                //Failsafe om användaren inte gör ett val mellan 1-3
             }
         }
         IO.println("Hoppas ni gillade spelet!");
@@ -40,8 +44,8 @@ public class Game {
     private Player createPlayer(int playerNumber) {//Själva player creating metoden
 
         IO.println("--Player " + playerNumber + "--");
-        String firstName = readInput("Ditt förnamn?: ");
-        String lastName = readInput("Ditt efternamn?: ");
+        String firstName = readInput("Ditt förnamn?: "); //Kallar på promt med try/catch
+        String lastName = readInput("Ditt efternamn?: "); //Kallar på promt med try/catch
         return new Player(firstName, lastName);
     }
 
@@ -92,6 +96,9 @@ public class Game {
 
     private boolean wantToPlayMore() {
         //Metoden för att få input från användare om de vill spela igen, med en safe i form av .trim().toLowerCase();
+        //Har inte fått med denna i själva koden och vet inte riktigt hur, det blev fel när jag skrev in menyn
+        //och vet inte hur jag fixar det helt, försökte skriva in den i Play men vet inte hur
+        //jag får den att starta menyn igen
 
         String answer = IO.readln("Vill du spela igen? Ja/Nej: ").trim().toLowerCase();
         return answer.equals("ja");
